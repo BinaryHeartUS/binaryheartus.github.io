89f834ae098fac07ea1e6bfde8f9a16043bd14ef
2tmb2 <tal@belkind.net>
2025-11-06 09:30:34 -0500

    Created WP homepage

--- src/App.tsx
+++ src/App.tsx
@@ -50,10 +50,10 @@ import NUJoin from './pages/nu/Join';
 
 // WP pages
 import WPHome from './pages/wp/Home';
-import WPAbout from './pages/wp/About';
-import WPContact from './pages/wp/Contact';
-import WPDonate from './pages/wp/Donate';
-import WPJoin from './pages/wp/Join';
+//import WPAbout from './pages/wp/About';
+//import WPContact from './pages/wp/Contact';
+//import WPDonate from './pages/wp/Donate';
+//import WPJoin from './pages/wp/Join';
 
 function AppContent() {
   // Update page title based on current route
@@ -117,10 +117,10 @@ function AppContent() {
 
         {/* WP Chapter routes */}
         <Route path="/wp" element={<WPHome />} />
-        <Route path="/wp/about" element={<WPAbout />} />
+        {/*<Route path="/wp/about" element={<WPAbout />} />
         <Route path="/wp/contact" element={<WPContact />} />
         <Route path="/wp/donate" element={<WPDonate />} />
-        <Route path="/wp/join" element={<WPJoin />} />
+        <Route path="/wp/join" element={<WPJoin />} />*/}
       </Routes>
       <Footer />
     </div>
