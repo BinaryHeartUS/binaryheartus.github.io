268edbb198e0b59bf09db86525a46045535d605e
2tmb2 <tal@belkind.net>
2025-12-25 17:43:57 -0500

    fix anthony's image url

--- src/pages/rose-hulman/About.tsx
+++ src/pages/rose-hulman/About.tsx
@@ -197,7 +197,7 @@ export default function About() {
               />
 
               <PictureCard
-                imageSrc="/assets/images/chapters/rose-hulman/people/AnthonyFlores.JPG"
+                imageSrc="/assets/images/chapters/rose-hulman/people/AnthonyFlores.jpg"
                 name="Anthony Flores"
                 role="Director of Membership"
               />
